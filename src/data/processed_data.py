9712ba894a1585b7e0ff3cfb16808e2580ade0fe
Alejandro Mildiner <alejandro.mildiner@gmail.com>
2020-01-06 10:46:19 -0300

    add predictive model: add line between methods

--- src/data/processed_data.py
+++ src/data/processed_data.py
@@ -23,9 +23,8 @@ def generate_train_dataset_lowerbound(dataset, split_date='2016-01-01', lower_bo
     dataset_aux = dataset[(dataset.Date < split_date) & (dataset.Date > lower_bound)]
     result_df = pd.DataFrame()
     result_df['Label'] = dataset_aux.Label
-    result_df['Index'] = dataset_aux.index
-    print(dataset_aux.columns)
-    dataset_aux = dataset_aux.drop(columns=['Tomorrow_Date', 'Tomorrow_Open', 'Label', 'Diff_Tomorrow_Open', 'Date', 'Yesterday_Date'])
+    result_df['Index'] = dataset_aux.index 
+    dataset_aux = dataset_aux.drop(columns=['Tomorrow_Date', 'Tomorrow_Open','Label', 'Diff_Tomorrow_Open', 'Date', 'Yesterday_Date']) 
     return dataset_aux, result_df
 
 
@@ -34,9 +33,9 @@ def generate_test_dataset(dataset, split_date='2016-01-01'):
     result_df = pd.DataFrame()
     result_df['Label'] = dataset_aux.Label
     result_df['Index'] = dataset_aux.index
-
-    dataset_aux = dataset_aux.drop(columns=['Tomorrow_Date', 'Tomorrow_Open', 'Label', 'Diff_Tomorrow_Open', 'Date', 'Yesterday_Date'])
+    dataset_aux = dataset_aux.drop(columns=['Tomorrow_Date', 'Tomorrow_Open','Label', 'Diff_Tomorrow_Open', 'Date', 'Yesterday_Date']) 
     return dataset_aux, result_df
+
 
 if __name__ == '__main__':
     dataset = process_dataset('Stocks', 'aapl.us.txt')
